0f11f2b2da62f050064d1d8a681a89369392ca33
root <samgodwin111@gmail.com>
2023-01-26 00:31:26 +0000

    My overall commit

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,9 @@
-mport { uploadPhoto, createUser } from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const p1 = uploadPhoto();
-  const p2 = createUser();
-
-  return Promise.all([p1, p2]).then((value) => {
-    console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
-  })
-    .catch(() => { console.log('Signup system offline'); });
+  return Promise.all([uploadPhoto(), createUser()]).then((data) => {
+    const res = Object.assign({}, ...data);
+    console.log(`${res.body} ${res.firstName} ${res.lastName}`);
+    // console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
+  }).catch(() => { console.log('Signup system offline'); });
 }
